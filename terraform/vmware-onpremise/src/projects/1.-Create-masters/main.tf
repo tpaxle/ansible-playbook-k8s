1b2edb588a09e504ba4f6243c952af949f23a608
tpaxle <tpaxle@gmail.com>
2021-11-28 17:12:40 +0100

    Test

--- terraform/vmware-onpremise/src/projects/1.-Create-masters/main.tf
+++ terraform/vmware-onpremise/src/projects/1.-Create-masters/main.tf
@@ -1,12 +1,70 @@
 module "master_server_1"{
     source = "../../modules/vm-clone"
 
-    vm_name             = "master-node-1"
+    vm_name             = "worker-node-1"
     vm_cpu              = var.master_cpu
     vm_ram              = var.master_ram
-    vm_hostname         = "master-node-1"
+    vm_hostname         = "worker-node-1"
     vm_domain           = var.domain
-    vm_ip               = "10.0.0.31"
+    vm_ip               = "10.0.0.34"
+    vm_netmask          = "24"
+    vm_gw               = "10.0.0.101"
+
+
+    resource_pool_id    = "${data.vsphere_compute_cluster.cluster2.resource_pool_id}"
+    datacenter_id       = "${data.vsphere_datastore.datastore2_1.id}"
+    guest_id            = "${data.vsphere_virtual_machine.template.guest_id}"
+    scsi_type           = "${data.vsphere_virtual_machine.template.scsi_type}"
+    vm_template_uuid    = "${data.vsphere_virtual_machine.template.id}"
+    
+    adapter_type        = "${data.vsphere_virtual_machine.template.network_interface_types[0]}"
+    network_id          = "${data.vsphere_network.network_2.id}"
+
+    disk_size           = "${data.vsphere_virtual_machine.template.disks.0.size}"
+    disk_eagerly        = "${data.vsphere_virtual_machine.template.disks.0.eagerly_scrub}"
+    thin_provisioned    = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"
+
+    default_tags	= var.default_tags
+}
+
+module "master_server_2"{
+    source = "../../modules/vm-clone"
+
+    vm_name             = "worker-node-2"
+    vm_cpu              = var.master_cpu
+    vm_ram              = var.master_ram
+    vm_hostname         = "worker-node-2"
+    vm_domain           = var.domain
+    vm_ip               = "10.0.0.35"
+    vm_netmask          = "24"
+    vm_gw               = "10.0.0.101"
+
+
+    resource_pool_id    = "${data.vsphere_compute_cluster.cluster2.resource_pool_id}"
+    datacenter_id       = "${data.vsphere_datastore.datastore2_1.id}"
+    guest_id            = "${data.vsphere_virtual_machine.template.guest_id}"
+    scsi_type           = "${data.vsphere_virtual_machine.template.scsi_type}"
+    vm_template_uuid    = "${data.vsphere_virtual_machine.template.id}"
+    
+    adapter_type        = "${data.vsphere_virtual_machine.template.network_interface_types[0]}"
+    network_id          = "${data.vsphere_network.network_2.id}"
+
+    disk_size           = "${data.vsphere_virtual_machine.template.disks.0.size}"
+    disk_eagerly        = "${data.vsphere_virtual_machine.template.disks.0.eagerly_scrub}"
+    thin_provisioned    = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"
+
+    default_tags	= var.default_tags
+}
+
+module "master_server_2"{
+    source = "../../modules/vm-clone"
+
+    vm_name             = "worker-node-3"
+    vm_cpu              = var.master_cpu
+    vm_ram              = var.master_ram
+    vm_hostname         = "worker-node-3"
+    vm_domain           = var.domain
+    vm_ip               = "10.0.0.36"
     vm_netmask          = "24"
     vm_gw               = "10.0.0.101"
 
---
+++ terraform/vmware-onpremise/src/projects/2.-Create-nodes/variables.tf
@@ -0,0 +1,57 @@
+variable "default_tags" {
+  description = "Default Tags"
+  type        = map(string)
+}
+
+variable "dc_name" {
+  description = "DC name"
+  default = "dc1"
+}
+
+variable "dc2_name" {
+  description = "DC name"
+  default = "dc2"
+}
+
+
+variable "dc_cluster_name" {
+  description = "DC name"
+  default = "cluster1"
+}
+variable "dc_cluster2_name" {
+  description = "DC name"
+  default = "cluster1"
+}
+
+variable "dc_template_name" {
+  description = "DC name"
+  default = "dc1"
+}
+
+variable "template_name"{
+    description = "template name"
+}
+
+variable "datastorage_name" {
+  description = "dtastorage name"
+  default = "storage1"
+}
+
+variable "datastorage2_name" {
+  description = "dtastorage name"
+  default = "storage2"
+}
+
+variable "master_cpu"{
+    description = "cpu"
+    default = 2
+}
+
+variable "master_ram"{
+    description = "ram"
+    default = "2048"
+}
+
+variable "domain" {
+    description = "hola"
+}
