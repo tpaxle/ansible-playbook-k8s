950b96102428006b69138042f5fb813fbb7147a9
tpaxle <tpaxle@gmail.com>
2021-11-28 13:54:20 +0100

    test

--- terraform/vmware-onpremise/src/projects/1.-Create-masters/main.tf
+++ terraform/vmware-onpremise/src/projects/1.-Create-masters/main.tf
@@ -9,18 +9,6 @@ resource "vsphere_virtual_machine" "vm" {
 
   scsi_type = "${data.vsphere_virtual_machine.template.scsi_type}"
 
-  network_interface {
-    network_id   = "${data.vsphere_network.network.id}"
-    adapter_type = "${data.vsphere_virtual_machine.template.network_interface_types[0]}"
-  }
-
-  disk {
-    label            = "disk0"
-    size             = "${data.vsphere_virtual_machine.template.disks.0.size}"
-    eagerly_scrub    = "${data.vsphere_virtual_machine.template.disks.0.eagerly_scrub}"
-    thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"
-  }
-
   clone {
     template_uuid = "${data.vsphere_virtual_machine.template.id}"
 
