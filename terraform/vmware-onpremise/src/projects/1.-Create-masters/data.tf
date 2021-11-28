919d8cc32e7836802f101ae8f6dcb568c3324e79
tpaxle <tpaxle@gmail.com>
2021-11-28 12:53:20 +0100

    Add network

--- terraform/vmware-onpremise/src/projects/1.-Create-masters/data.tf
+++ terraform/vmware-onpremise/src/projects/1.-Create-masters/data.tf
@@ -6,3 +6,13 @@ data "vsphere_datastore" "datastore" {
   name          = var.datastorage_name
   datacenter_id = data.vsphere_datacenter.dc.id
 }
+
+data "vsphere_resource_pool" "pool" {
+  name          = "cluster1/Resources"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "network" {
+  name          = "VM Network"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
